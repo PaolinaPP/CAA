f2384c2464f8e5a11ddd46438c9f4fe39bfc269a
Paolina Petkova <paolina12123@gmail.com>
2019-05-14 11:08:33 +0300

    Add files via upload

--- 5zadacha.c
+++ 5zadacha.c
@@ -1,23 +1,28 @@
 #include <stdio.h> 
 int arr[] ={1, 2, 0, 0, 5, 6,0 ,20 ,1}; //an initialized array
 
-//a func to swap zeroes with the last non zero elements
-void swapZeroes() {
-	int i, k=(sizeof(arr)/4)-1, c;
-	for(i=0;i<k;i++){
-		if(arr[i]==0){
-			 arr[i] = arr[k];
-		     arr[k] = 0;
-		     k--;
-		}
-	}
-	//print the new array
-	for(i=0;i<(sizeof(arr)/4);i++){
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-}
+
+void pushZerosToEnd() 
+{ 
+    int count = 0;  // Count of non-zero elements 
+  
+    // Traverse the array. If element encountered is non- 
+    // zero, then replace the element at index 'count'  
+    // with this element 
+    for (int i = 0; i < (sizeof(arr)/4); i++) 
+        if (arr[i] != 0) 
+            arr[count++] = arr[i]; // here count is  
+                                   // incremented 
+  
+    // Now all non-zero elements have been shifted to  
+    // front and  'count' is set as index of first 0.  
+    // Make all elements 0 from count to end. 
+    while (count < (sizeof(arr)/4)) 
+        arr[count++] = 0; 
+} 
 int main() { 
-    swapZeroes();
+    pushZerosToEnd();
+    for (int i = 0; i < (sizeof(arr)/4); i++) 
+        printf("%d ", arr[i]); 
     return 0; 
 } 
